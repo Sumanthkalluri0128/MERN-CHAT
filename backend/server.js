7c76ce0ea3afd15b175b0cd5d0d047bec92039cd
sumanthkaluri0128 <sumanthkalluri0128@gmail.com>
2025-04-21 11:19:45 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const http = require("http");
+const socketio = require("socket.io");
+const userRouter = require("./routes/userRoutes");
+const socketIo = require("./socket");
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+    method: ["GET", "POST"],
+    Credentials: true,
+  },
+});
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connected to DB"))
+  .catch((err) => console.log("Mongodb connected failed", err));
+
+//initialize
+socketIo(io);
+//our route
+app.use("/api/users", userRouter);
+
+//start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, console.log("server is up and running on port", PORT));
